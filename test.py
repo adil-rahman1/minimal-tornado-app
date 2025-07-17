c01c9b0fd03be739a9ab87dbc3b6449c4021efde
Adil Rahman <arahman.99@outlook.com>
2025-07-17 11:30:44 +0100

    Add test for links in index

--- test.py
+++ test.py
@@ -9,12 +9,16 @@ class TestMainHandler(AsyncHTTPTestCase):
 
     def test_background_colour_changer(self):
         response = self.fetch('/')
-        self.assertEqual(response.code, 200)
         self.assertIn(b"Change my background color by clicking the button below", response.body)
 
     def test_nav_in_index(self):
         response = self.fetch('/')
         self.assertIn(b"<nav>", response.body)
+
+    def test_links_in_index(self):
+        response = self.fetch('/')
+        content = response.body.decode(encoding='utf-8')
+        self.assertEqual(content.count("<li>"), 4)
 
 if __name__ == "__main__":
     unittest.main()
